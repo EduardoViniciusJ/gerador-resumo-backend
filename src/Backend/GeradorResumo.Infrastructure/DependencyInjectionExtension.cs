0204adc9c3512af1d40ffe8cd5a32bbc9ba2fd3a
Eduardo <eduardoviniciusjorge@gmail.com>
2025-12-02 11:35:08 -0300

    feat: add package Google.GenAI and configuration

---
+++ src/Backend/GeradorResumo.Infrastructure/DependencyInjectionExtension.cs
@@ -0,0 +1,26 @@
+﻿using Google.GenAI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace GeradorResumo.Infrastructure
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddInfrastructure(this IServiceCollection service, IConfiguration configuration)
+        {
+            AddGoogleGenAI(service, configuration); 
+        }
+
+        public static void AddGoogleGenAI(this IServiceCollection service, IConfiguration configuration)
+        {
+            var settings = configuration["GoogleGenAI:ApiKey"];
+
+            if(string.IsNullOrWhiteSpace(settings)) {
+                throw new Exception("falta chave da api");
+            }
+
+            service.AddSingleton(new Client(apiKey: settings));
+        }
+    }
+}
